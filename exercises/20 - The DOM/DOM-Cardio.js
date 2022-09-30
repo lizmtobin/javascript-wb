22786980ad7f8d30f7dde13d045555dffb5276b8
liz tobin <lizmtobin@gmail.com>
2022-09-30 17:00:12 +0100

    add card style

--- exercises/20 - The DOM/DOM-Cardio.js
+++ exercises/20 - The DOM/DOM-Cardio.js
@@ -2,10 +2,8 @@
 const div = document.createElement("div");
 // add a class of wrapper to it
 div.classList.add("wrapper");
-console.log(div);
 // put it into the body
 document.body.appendChild(div);
-console.log(document.body);
 // make an unordered list
 const ul = `<ul>
   <li>one</li>
@@ -16,8 +14,6 @@ const ul = `<ul>
 // add three list items with the words "one, two, three" in them
 // put that list into the above wrapper
 div.innerHTML = ul;
-console.log(div);
-
 // create an image
 const img = document.createElement("img");
 // set the source to an image
@@ -52,18 +48,37 @@ pElement[1].classList.add("warning");
 // remove the first paragraph
 pElement[0].remove();
 // create a function called generatePlayerCard that takes in three arguments: name, age, and height
-
 // have that function return html that looks like this:
 // <div class="playerCard">
 //   <h2>NAME — AGE</h2>
 //   <p>They are HEIGHT and AGE years old. In Dog years this person would be AGEINDOGYEARS. That would be a tall dog!</p>
 // </div>
 
+function generatePlayerCard(name, age, height) {
+  const html = `
+  <div class="playerCard">
+    <h2>${name} — ${age}</h2>
+    <p>They are ${height} and ${age} years old. In Dog years this person would be ${
+    age * 7
+  }. That would be a tall dog!</p>
+    <button class="delete" type="button">&times; Delete</button>
+  </div>
+  `;
+  return html;
+}
 // make a new div with a class of cards
-
+const cards = document.createElement("div");
+cards.classList.add("cards");
 // make 4 player cards using generatePlayerCard
+let cardsHTML = generatePlayerCard("Wes", 12, 150);
+cardsHTML += generatePlayerCard("Scott", 123, 150);
+cardsHTML += generatePlayerCard("Snickers", 6, 150);
+cardsHTML += generatePlayerCard("Poppy", 2, 150);
 
 // append those cards to the div
+cards.innerHTML = cardsHTML;
+div.insertAdjacentElement("beforebegin", cards);
+
 // put the div into the DOM just before the wrapper element
 // Bonus, put a delete Button on each card so when you click it, the whole card is removed
 
